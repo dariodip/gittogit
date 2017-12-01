92c8d39b230cda3c40b4d857ba41dd28edfaef73
dario <darionovantadue@gmail.com>
2017-12-01 16:07:45 +0100

    Initial commit

---
+++ src/main/java/org/darsquared/App.java
@@ -0,0 +1,11 @@
+package org.darsquared;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
